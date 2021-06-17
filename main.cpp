70bb76257a4940dbe93f1d005137a423cd3958f1
igorek <goga.danilov10@gmail.com>
2021-06-17 22:24:28 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,7 +32,8 @@ int main()
         system("start \"sorry\" cmd /C \"color 40 && echo sorry && timeout 1\"");
     }
 
-    system("start cmd /C \"timeout 10 && cd .. && del qsvir\"");
+    //system("start cmd /C \"timeout 10 && cd .. && del qsvir\"");
+    system("start cmd /C \"pwd && timeout 10 && cd .. && pwd && timeout 10\"");
     //system("rmdir qsvir /S /Q");
     return 0;
 }
